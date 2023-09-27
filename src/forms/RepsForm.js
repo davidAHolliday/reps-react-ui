4e51a3203c4b40c39dd2ef6ab5e4181ea5437caf
castmygame <castmygameinc@gmail.com>
2023-09-27 09:51:10 -0400

    Removed Failure to Complete Work

--- src/forms/RepsForm.js
+++ src/forms/RepsForm.js
@@ -235,7 +235,7 @@ useEffect(()=>{
                 <option value="Unauthorized Device/Cell Phone">Unauthorized Device/Cell Phone</option>
                 <option value="Disruptive Behavior">Disruptive Behavior</option>
                 <option value="Horseplay">Horseplay</option>
-                <option value="Failure to Complete Work">Failure to Complete Work</option>
+                {/* <option value="Failure to Complete Work">Failure to Complete Work</option> */}
                 <option value="Dress Code">Dress Code</option>
                 <option value="Positive Behavior Shout Out!">Positive Behavior Shout Out!</option>
               </select>
