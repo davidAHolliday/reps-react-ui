22319716f8c7be2bff35e9dba119f3a6d0aaa686
castmygame <castmygameinc@gmail.com>
2023-10-15 20:34:34 -0400

    Pointed to backend

--- src/utils/jsonData.js
+++ src/utils/jsonData.js
@@ -1,6 +1,6 @@
 // export const baseUrl ="https://reps-react-demo.wn.r.appspot.com"
-export const baseUrl = "http://localhost:8080"
-// export const baseUrl = "https://repsdms.ue.r.appspot.com"
+// export const baseUrl = "http://localhost:8080"
+export const baseUrl = "https://repsdms.ue.r.appspot.com"
 
 export const essayData = {
 
