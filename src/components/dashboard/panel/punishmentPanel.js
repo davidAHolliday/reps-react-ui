da84576401e6f16ac16ae2ad3a42ba60b21ee1a3
David Anthony Perez <107569920+dperezskillstorm@users.noreply.github.com>
2023-12-03 19:50:53 -0600

    punish ent and student panels pull from data base and not dummy data

--- src/components/dashboard/panel/punishmentPanel.js
+++ src/components/dashboard/panel/punishmentPanel.js
@@ -1,21 +1,47 @@
-import react from 'react'
+import react, {useEffect,useState} from 'react'
 import { Table, TableContainer, TableHead, TableBody, TableRow, TableCell, Paper } from '@mui/material';
 import Typography from '@mui/material/Typography';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import ContactsIcon from '@mui/icons-material/Contacts';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import jsonData from "../../data.json"
+import axios from "axios"
+import { baseUrl } from '../../../utils/jsonData'
+
+
 
 
 
 
    const PunishmentPanel = () => {
 
-	const data = jsonData.punishment;
+    const [listOfPunishments, setListOfPunishments]= useState([])
+
+    const headers = {
+      Authorization: "Bearer " + sessionStorage.getItem("Authorization"),
+    };
+    
+    const url = `${baseUrl}/punish/v1/punishments`; // Replace with your actual API endpoint
+    
+
+    useEffect(() => {
+      axios
+        .get(url, { headers }) // Pass the headers option with the JWT token
+        .then(function (response) {
+          setListOfPunishments(response.data);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }, []);
+
+	  const data = jsonData.punishment
 
     const hasScroll = data.length > 10;
     return (
         <>
+                 { console.log(listOfPunishments)}
+
          <div style={{backgroundColor:"rgb(25, 118, 210)",marginTop:"10px", marginBlock:"5px"}}>
    <Typography color="white" variant="h6" style={{ flexGrow: 1, outline:"1px solid  white",padding:
 "5px"}}>
