69ae3115ed4ebbc3097fbc7835c1a7510b82042b
David Anthony Perez <107569920+dperezskillstorm@users.noreply.github.com>
2023-12-27 18:41:30 -0600

    updated heatmap tags in admin dash

--- src/components/dashboard/admin/adminPunishmentPanel.js
+++ src/components/dashboard/admin/adminPunishmentPanel.js
@@ -1,11 +1,12 @@
-import react, {useEffect,useState} from 'react'
+import React, {useEffect,useState} from 'react'
 import { Table, TableContainer, TableHead, TableBody, TableRow, TableCell, Paper } from '@mui/material';
 import Typography from '@mui/material/Typography';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import ContactsIcon from '@mui/icons-material/Contacts';
-import VisibilityIcon from '@mui/icons-material/Visibility';
 import axios from "axios"
 import { baseUrl } from '../../../utils/jsonData'
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+import CircularProgress from '@mui/material/CircularProgress';
 
 
 
@@ -13,10 +14,16 @@ import { baseUrl } from '../../../utils/jsonData'
 
 
    const AdminPunishmentPanel = ({filter}) => {
+    const Alert = React.forwardRef(function Alert(props, ref) {
+      return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+    });
 
     const [listOfPunishments, setListOfPunishments]= useState([])
     const [filterData, setFilterData] = useState();
     const [sort,setSort] = useState("");
+    const [loadingPunihsmentId, setLoadingPunishmentId] = useState({id:null,buttonType:""});
+    const [toast,setToast] = useState({visible:false,message:""})
+
 
     const headers = {
       Authorization: "Bearer " + sessionStorage.getItem("Authorization"),
@@ -39,7 +46,7 @@ useEffect(()=>{
         .catch(function (error) {
           console.log(error);
         });
-    }, []);
+    }, [toast.visible]);
 
 
     // if(sort == "ALL"){
@@ -65,69 +72,170 @@ useEffect(()=>{
       const daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24)); // Convert milliseconds to days
       return daysDifference;
     };
+
+    const handleClose = (event, reason) => {
+      if (reason === 'clickaway') {
+        return;
+      }
+  
+      setToast(false);
+    };
+
+    const handleClosePunishment = (obj) =>{
+      setLoadingPunishmentId({id:obj.punishmentId,buttonType:"close"})
+      const url = `${baseUrl}/punish/v1/close/${obj.punishmentId}`;
+      axios
+      .post(url,[], { headers }) // Pass the headers option with the JWT token
+      .then(function (response) {
+        console.log(response)
+        setToast({visible:true,message:"Your Referral was closed"})
+
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .finally(()=>{
+        setTimeout(()=>{
+          setToast({visible:false,message:""})
+          setLoadingPunishmentId({id:null,buttonType:""})
+
+        },1000)
+      }
+
+      );
+  };
+
+  const handleDeletePunishment = (obj) =>{
+    setLoadingPunishmentId({id:obj.punishmentId,buttonType:"delete"})
+  
+    const url = `${baseUrl}/punish/v1/delete`;
+    axios
+    .delete(url, { headers:headers,data:obj }) // Pass the headers option with the JWT token
+    .then(function (response) {
+      console.log(response)
+  setToast({visible:true,message:"Your Referral was Deleted"})
+  
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+    .finally(()=>{
+      setTimeout(()=>{
+        setToast({visible:false,message:""})
+        setLoadingPunishmentId({id:null,buttonType:""})
+      },1000)
+    }
+      );
+};
     
     return (
-        <>
-                 { console.log(listOfPunishments)}
+      <>
+               { console.log(listOfPunishments)}
 
-         <div style={{backgroundColor:"rgb(25, 118, 210)",marginTop:"10px", marginBlock:"5px"}}>
-   <Typography color="white" variant="h6" style={{ flexGrow: 1, outline:"1px solid  white",padding:
+       <div style={{backgroundColor:"rgb(25, 118, 210)",marginTop:"10px", marginBlock:"5px"}}>
+ <Typography color="white" variant="h6" style={{ flexGrow: 1, outline:"1px solid  white",padding:
 "5px"}}>
 
 
-        </Typography>
-        </div>
-   
-        <TableContainer component={Paper} style={{ maxHeight: hasScroll ? '400px' : 'auto', overflowY: hasScroll ? 'scroll' : 'visible' }}>
-        <Table>
-          <TableHead>
-            {/* ... (previous code) */}
-          </TableHead>
-          <TableBody>
-            {data.length > 0 ? (
-              data.map((x, key) => {
-                const days = calculateDaysSince(x.timeCreated);
+      </Typography>
+      </div>
+      <Snackbar anchorOrigin={{ vertical: 'top', horizontal: 'center' }} open={toast.visible} autoHideDuration={6000} onClose={handleClose}>
+<Alert Close={handleClose} severity="success" sx={{ width: '100%' }}>
+{toast.message}
+</Alert>
+</Snackbar>
+      <TableContainer component={Paper} style={{ maxHeight: hasScroll ? '400px' : 'auto', overflowY: hasScroll ? 'scroll' : 'visible' }}>
+      <Table>
+      <TableHead>
+      <TableRow>
+        <TableCell variant="head" style={{ fontWeight: 'bold' }}>
+          Name
+        </TableCell>
+        <TableCell variant="head" style={{ fontWeight: 'bold' }}>
+         Referal Type
+        </TableCell>
+        <TableCell variant="head" style={{ fontWeight: 'bold' }}>
+          Description
+        </TableCell>
+        <TableCell variant="head" style={{ fontWeight: 'bold' }}>
+          Level
+        </TableCell>
+        <TableCell variant="head" style={{ fontWeight: 'bold' }}>
+         Status
+        </TableCell>
+        <TableCell variant="head" style={{ fontWeight: 'bold' }}>
+         Days Since
+        </TableCell>
+        <TableCell variant="head" style={{ fontWeight: 'bold' }}>
+         Action
+        </TableCell>
 
-                return (
-                  <TableRow
-                    style={{
-                      backgroundColor: days >= 4 ? "#A020F0" : days >= 3 ? "#FF402C" : days >= 2 ? "#FFE366" : "#00FF00",
-                    }}
-                    key={key}
-                  >
-                    <TableCell>
-                      <div style={{ display: 'flex', alignItems: 'center' }}>
-                        <AccountCircleIcon
-                          style={{
-                            fontSize: '2rem',  // Adjust the size as needed
-                            color: 'rgb(25, 118, 210)', // Change the color to blue
-                          }}
-                        />
-                        <span>{x.student.firstName} {x.student.lastName}</span>
-                      </div>
-                    </TableCell>
-                    <TableCell>{x.infraction.infractionName}</TableCell>
-                    <TableCell>{x.infraction.infractionDescription}</TableCell>
-                    <TableCell>{x.infraction.infractionLevel}</TableCell>
-                    <TableCell>{x.status}</TableCell>
-                    <TableCell>{days}</TableCell>
-                    <TableCell>
-                      <ContactsIcon color="primary" />
-                      <VisibilityIcon color="primary" />
-                    </TableCell>
-                  </TableRow>
-                );
-              })
-            ) : (
-              <TableRow>
-                <TableCell colSpan="5">No open assignments found.</TableCell>
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </>
-  );
+      </TableRow>
+    </TableHead>
+        <TableBody>
+          {data.length > 0 ? (
+            data.map((x, key) => {
+              const days = calculateDaysSince(x.timeCreated);
+
+              return (
+                <TableRow
+
+                  key={key}
+                >
+                  <TableCell>
+                    <div style={{ display: 'flex', alignItems: 'center' }}>
+                      <AccountCircleIcon
+                        style={{
+                          fontSize: '2rem',  // Adjust the size as needed
+                          color: 'rgb(25, 118, 210)', // Change the color to blue
+                        }}
+                      />
+                      <span>{x.student.firstName} {x.student.lastName}</span>
+                    </div>
+                  </TableCell>
+                  <TableCell>{x.infraction.infractionName}</TableCell>
+                  <TableCell style={{width:"75px"}}>{x.infraction.infractionDescription}</TableCell>
+                  <TableCell>{x.infraction.infractionLevel}</TableCell>
+                  <TableCell>
+<div 
+className={`status-tag ${days >= 4 ? "tag-critical" : days >= 3 ? "tag-danger" : days >= 2 ? "tag-warning" : "tag-good"}`}
+>
+{x.status}
+</div>
+</TableCell>
+
+                  <TableCell>{days}</TableCell>
+                  <TableCell>
+<button style={{height:"50px", width:"100px"}} onClick={() => { handleClosePunishment(x) }}>
+{(loadingPunihsmentId.id === x.punishmentId && loadingPunihsmentId.buttonType==="close") ? (
+  <CircularProgress style={{height:"20px", width:"20px"}} color="secondary" />
+) : (
+  "Mark complete"
+)}
+</button>
+<hr/>
+<button style={{height:"50px", width:"100px",backgroundColor:"red"}} onClick={() => { handleDeletePunishment(x) }}>
+{(loadingPunihsmentId.id === x.punishmentId && loadingPunihsmentId.buttonType==="delete") ? (
+  <CircularProgress style={{height:"20px", width:"20px"}} color="secondary" />
+) : (
+  "Delete"
+)}
+</button>
+
+</TableCell>
+                </TableRow>
+              );
+            })
+          ) : (
+            <TableRow>
+              <TableCell colSpan="5">No open assignments found.</TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  </>
+);
 };
 
 export default AdminPunishmentPanel;
