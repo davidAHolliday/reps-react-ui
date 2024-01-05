98f695356c036311cd7876d04b83a07a87e81f50
David Anthony Perez <107569920+dperezskillstorm@users.noreply.github.com>
2024-01-04 23:07:51 -0600

    added week range adj

--- src/components/dashboard/teacher/teacherPanels/referralsByWeek.js
+++ src/components/dashboard/teacher/teacherPanels/referralsByWeek.js
@@ -1,9 +1,10 @@
 import { LineChart } from '@mui/x-charts/LineChart';
 import { Typography } from '@mui/material';
 import { extractDataByWeek, getCurrentWeekOfYear } from '../../global/helperFunctions';
+import { useState } from 'react';
 
 export default function TotalReferalByWeek({data = []}) {
-//   const [punishmentData,setPunishmentData] = useState([])
+  const [rangeWeeks,setRangeWeek] = useState(10);
   const currentWeek = getCurrentWeekOfYear();
 
 //This helps adjust the week number if current week extend prior to this year
@@ -16,7 +17,6 @@ if(cw <=0){
 }
 
 
-const rangeWeeks = 10
 
 const GenerateChartData = (currentWeek, rangeWeeks,data) => {
   const genData = [];
@@ -52,7 +52,7 @@ const seriesData = displayDate.map(obj => Object.values(obj)[0] || 0); // Extrac
      data && (<>
       <Typography variant="h6" gutterBottom>
        Number of Referrals By Week
-      </Typography>
+      </Typography> <button onClick={()=>setRangeWeek((prev)=> prev-1)} style={{height:"20px", width:"20px",padding:0,borderRadius:0}}>-</button> <button onClick={()=>setRangeWeek((prev)=> prev+1)} style={{height:"20px", width:"20px",padding:0, borderRadius:0}}>+</button> 
       <LineChart
         xAxis={[{ 
           scaleType:'band', data: xAxisData, label:"Weeks"
