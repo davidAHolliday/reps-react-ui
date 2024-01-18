6ad2dccfebe36e0d7a5acf306023dda8cb8f11a5
David Anthony Perez <107569920+dperezskillstorm@users.noreply.github.com>
2024-01-17 22:39:21 -0600

    set points to zero if data not loaded

--- src/components/dashboard/student/positivePointsComponents.js
+++ src/components/dashboard/student/positivePointsComponents.js
@@ -1,14 +1,11 @@
-import react from 'react'
-
-const TotalPositivePoints = ({data}) =>{
+const TotalPositivePoints = ({ data = { points: 0 } }) => {
     return (
-        <>
-                <h3>Total Positive Points Component</h3>
+      <>
+        <h3>Total Positive Points Component</h3>
         <h1>{data.points}</h1>
-        </>
-    )
-
-    
-}
-
- export default TotalPositivePoints;
+      </>
+    );
+  };
+  
+  export default TotalPositivePoints;
+  
