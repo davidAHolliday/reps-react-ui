e89db0cece4cc270f6ad535fbf26a38f1faa8302
castmygame <castmygameinc@gmail.com>
2024-01-22 21:11:26 -0500

    Pop up working in overview panel, redirect not finished

--- src/components/dashboard/teacher/teacherPanels/teacherOverview.js
+++ src/components/dashboard/teacher/teacherPanels/teacherOverview.js
@@ -18,6 +18,7 @@ import TeacherShoutOutWidget from './teacherShoutOutWidget.js';
 import LevelThreePanel from '../../global/levelThreePanel.js';
 import Button from '@mui/material/Button';
 
+
    const TeacherOverviewPanel = ({data = []}) => {
 	const [listOfStudents, setListOfStudents]= useState([])
   const [studentDisplay, setStudentDisplay] = useState(false);
@@ -26,23 +27,23 @@ import Button from '@mui/material/Button';
   const [openModal, setOpenModal] = useState({display:false,message:"",buttonType:""})
   const [status, setStatus] = useState([])
 
+  const filterPunishmentByStatus = (data) => {
+    return data.filter(x => x.status === "PENDING");
+  }
 
-    const isFound = data.some(element => {
-      if(element.status === "PENDING") {
-        return true;
-      }
-      return false;
-    })
-
-    if(isFound) {
+  useEffect(() => {
+    filterPunishmentByStatus(data)
+    if(data.length > 0){
       setOpenModal({display:true, message:"Attention! You have level 3 punishments with student answers that must be reviewed before closing. Please hit the redirect button to go to the level 3 approval page or cancel to continue. You will receive notifications until the answers are reviewed as they are not Closed until you review. Thank you!", buttonType:"redirect"});
     }
+  }, [data]);
+
 
 
     return (
         <>
-        {console.log(isFound)}
-        {openModal.display && <div className="modal-overlay">
+            {console.log(data + "These are the data!")}
+    {openModal.display && <div className="modal-overlay">
   <div className="modal-content">
     <div className='modal-header'>
       <h3>{openModal.message}</h3>
--- src/components/notification-bar/NotificationBar.js
+++ src/components/notification-bar/NotificationBar.js
@@ -1,9 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 import "./notification-bar.css"
 export const NotificationBar = (props) => {
 
   const [data,setData] = useState([])
+  // const [openModal, setOpenModal] = useState({display:false,message:"",buttonType:""})
+
+  // const filterPunishmentByStatus = (props) => {
+  //   return props.filter(x => x.status === "PENDING");
+  // }
+
+  // useEffect(() => {
+  //   filterPunishmentByStatus(props)
+  //   if(props.length > 0){
+  //     setOpenModal({display:true, message:"Attention! You have level 3 punishments with student answers that must be reviewed before closing. Please hit the redirect button to go to the level 3 approval page or cancel to continue. You will receive notifications until the answers are reviewed as they are not Closed until you review. Thank you!", buttonType:"redirect"});
+  //   }
+  // }, [props]);
 
   useEffect(() => {
     const notificationData = [
@@ -49,6 +62,32 @@ export const NotificationBar = (props) => {
   }
 
   return (
+    <>
+    {console.log(props + "These are the props!")}
+    {openModal.display && <div className="modal-overlay">
+  <div className="modal-content">
+    <div className='modal-header'>
+      <h3>{openModal.message}</h3>
+    </div>
+    <div className='modal-body'>
+    </div>
+    <div className='modal-buttons'>
+
+      <button onClick={() => {
+        setOpenModal({display:false,message:""})}}>Cancel</button>
+      {openModal.buttonType==="redirect" && <Button
+      type="redirect"
+      onClick={() => {
+        setOpenModal({display:false,message:""})}}
+      width='50%'
+      variant="contained"
+      sx={{ height: '100%' }} // Set explicit height
+    >
+      Submit
+    </Button>}
+   </div>
+  </div>
+</div>}
     
 <div className='notification-body'>
 <div className="popup-container">
@@ -68,7 +107,7 @@ export const NotificationBar = (props) => {
   )}
 </div>
 </div>
-
+</>
 
     
   );
