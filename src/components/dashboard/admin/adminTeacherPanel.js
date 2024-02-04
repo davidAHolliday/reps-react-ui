c08796ae3113b1129f8d1529cc7fb13a4065028d
David Anthony Perez <107569920+dperezskillstorm@users.noreply.github.com>
2024-02-04 12:44:09 -0600

    updated profile teacher with info

--- src/components/dashboard/admin/adminTeacherPanel.js
+++ src/components/dashboard/admin/adminTeacherPanel.js
@@ -1,18 +1,21 @@
-import react, {useState,useEffect} from 'react'
+import react, {useState,useEffect,useRef} from 'react'
 import { Table, TableContainer, TableHead, TableBody, TableRow, TableCell, Paper,Card } from '@mui/material';
 import Typography from '@mui/material/Typography';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import axios from "axios"
 import { baseUrl } from '../../../utils/jsonData'
-import StudentProfile from '../../StudentProfile';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import jsPDF from 'jspdf';
+import 'jspdf-autotable';
 
    const AdminTeacherPanel = () => {
 
 
 	const [data, setData]= useState([])
-  const [studentDisplay, setStudentDisplay] = useState(false);
-  const [studentEmail, setStudentEmail] = useState("");
-  const [studentName, setStudentName] = useState("");
+  const [teacherProfileModal,setTeacherProfileModal] = useState(false)
+  const [teacherProfileData, setTeacherProfileData] = useState([])
+  const [activeTeacher, setActiveTeacher] = useState();
+
 
     const headers = {
       Authorization: "Bearer " + sessionStorage.getItem("Authorization"),
@@ -35,9 +38,174 @@ import StudentProfile from '../../StudentProfile';
 
 
 
+//Fetch Teacher Data 
+
+const fetchTeacherData = (teacherEmail) =>{
+  //Replace with teacherendpoint
+  const punishUrl= `${baseUrl}/punish/v1/punishments/`;
+  axios
+  .get(punishUrl, { headers }) // Pass the headers option with the JWT token
+  .then(function (response) {
+
+    const data = response.data;
+    console.log(teacherEmail)
+    const newData = data.filter(((x)=> x.teacherEmail === teacherEmail));
+    console.log("find me ", newData)
+    setTeacherProfileData(newData);
+    setTeacherProfileModal(true);  
+    
+console.log(newData)
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
+  
+
+const handleProfileClick = (x) =>{
+  setActiveTeacher(x)
+  fetchTeacherData(x.email)
+  setTeacherProfileModal(true)
+}
+
+const pdfRef = useRef();
+
+const generatePDF = (studentData) => {
+  const pdf = new jsPDF();
+    // Add logo
+    const logoWidth = 50; // Adjust the width of the logo as needed
+    const logoHeight = 50; // Adjust the height of the logo as needed
+    const logoX = 130; // Adjust the X coordinate of the logo as needed
+    const logoY = 15; // Adjust the Y coordinate of the logo as needed
+    
+//https://medium.com/dont-leave-me-out-in-the-code/5-steps-to-create-a-pdf-in-react-using-jspdf-1af182b56cee
+//Resource for adding image and how pdf text works
+    var image = new Image();
+    image.src = "/burke-logo.png";
+    pdf.addImage(image, 'PNG', logoX,logoY , logoWidth, logoHeight);
+
+  // Add student details section
+  pdf.setFontSize(12)
+  pdf.rect(15,15,180,50)
+  pdf.text(`${studentData[0].student.firstName} ${studentData[0].student.lastName}`, 20, 20);
+  pdf.text(`Email: ${studentData[0].student.studentEmail}`, 20, 30);
+  pdf.text(`Phone: ${studentData[0].student.studentPhoneNumber}`, 20, 40);
+  pdf.text(`Grade: ${studentData[0].student.grade}`, 20, 50);
+  pdf.text(`Address: ${studentData[0].student.address}`, 20, 60);
+
+  // Add punishment details table
+  pdf.autoTable({
+    startY: 70, // Adjust the Y-coordinate as needed
+    head: [['Status', 'Description', 'Date', 'Infraction']],
+    body: studentData.map((student) => [
+      student.status,
+      student.infraction.infractionDescription,
+      student.timeCreated,
+      student.infraction.infractionName,
+    ]),
+  });
+
+  // Save or open the PDF
+  pdf.save('student_report.pdf');
+};
+
+
     const hasScroll = data.length > 10;
     return (
         <>
+          {(teacherProfileModal && teacherProfileData) && (
+
+
+<div style={{
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  position: 'fixed',
+  top: 0,
+  left: 0,
+  width: '100%',
+  height: '100%',
+}} className="modal-overlay">
+  <div style={{
+    height: '70%',
+    width: '80%',
+    position: 'relative',
+    backgroundColor: 'white',  // Adjust background color as needed
+    padding: '20px',          // Adjust padding as needed
+    borderRadius: '8px',      // Add border radius as needed
+    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',  // Add box shadow as needed
+  }} className="modal-content">
+      <div className='modal-header'>
+        <div style={{display:"flex", flexDirection:"row"}}>
+            <div className='box-left'>
+
+              <div className='student-info-box'>
+              <h4 style={{ textAlign: "left" }}>{activeTeacher.firstName} {activeTeacher.lastName}</h4>
+                <div className='details-box'>
+                  <div style={{ textAlign: "left" }}>Email: {activeTeacher.email}</div>
+                  <div style={{ textAlign: "left" }}>Address: {}</div>
+                </div>
+            </div>
+            </div>
+
+            <div style={{  width: "100px", height: "100px", marginLeft: "auto" }} className='box-right'>
+        <AccountBoxIcon sx={{ fontSize: 100 }}/>
+            
+            </div>
+      
+      </div>
+      </div>
+
+    <div style={{height:"320px"}}className='modal-body-student'>
+  <TableContainer style={{height:"300px",backgroundColor:"white" }}>
+    <Table stickyHeader>
+    <TableHead>
+      <TableRow>
+        <TableCell style={{ width: "25%" }}>
+          Status
+        </TableCell>
+        <TableCell style={{ width: "25%" }}>
+          Description
+        </TableCell>
+        <TableCell style={{ width: "25%" }}>
+          Date
+        </TableCell>
+        <TableCell style={{ width: "25%" }}>
+          Infraction
+        </TableCell>
+      </TableRow>
+    </TableHead>
+    <TableBody >
+      {teacherProfileData.map((teacher, index) => (
+        <TableRow style={{ background: index % 2 === 0 ? "lightgrey" : "white" }} key={index}>
+          <TableCell style={{ width: "25%" }}>
+            {teacher.status}
+          </TableCell>
+          <TableCell style={{ width: "25%" }}>
+            {teacher.infraction.infractionDescription}
+          </TableCell>
+          <TableCell style={{ width: "25%" }}>
+            {teacher.timeCreated}
+          </TableCell>
+          <TableCell style={{ width: "25%" }}>
+            {teacher.infraction.infractionName}
+          </TableCell>
+        </TableRow>
+      ))}
+    </TableBody>
+    </Table>
+  </TableContainer>
+</div>
+
+
+      <div style={{padding:"10px"}} className='modal-buttons'>
+        <button onClick={() => { setTeacherProfileModal(false) }}>Cancel</button>
+        <button onClick={()=>{generatePDF(teacherProfileData)}}style={{backgroundColor:"#CF9FFF"}} >Print</button>
+
+      </div>
+    </div>
+  </div>
+)}
          <div style={{backgroundColor:"rgb(25, 118, 210)",marginTop:"10px", marginBlock:"5px"}}>
    <Typography color="white" variant="h6" style={{ flexGrow: 1, outline:"1px solid  white",padding:
 "5px"}}>
@@ -67,7 +235,7 @@ import StudentProfile from '../../StudentProfile';
 
           {data.length > 0 ? (
             data.map((x, key) => (
-<TableRow key={key} onClick={() => {setStudentDisplay(true); setStudentEmail(x.username); setStudentName(x.firstName);}}>
+<TableRow key={key} onClick={() => {handleProfileClick(x)}}>
   <TableCell>
     <div style={{ display: 'flex', alignItems: 'center' }}>
       <AccountCircleIcon
@@ -94,7 +262,6 @@ import StudentProfile from '../../StudentProfile';
       </Table>
    
     </TableContainer>
-    {studentDisplay && <StudentProfile studentEmail={studentEmail} studentName={studentName}/>}
 
 
     </>
