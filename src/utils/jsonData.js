53d2c25a9a092512638a2428083d320cd156ab99
David Anthony Perez <107569920+dperezskillstorm@users.noreply.github.com>
2024-02-20 20:03:30 -0600

    addded to prod

--- src/utils/jsonData.js
+++ src/utils/jsonData.js
@@ -1,7 +1,7 @@
 // #local
-export const baseUrl = "http://localhost:8080"
+// export const baseUrl = "http://localhost:8080"
 // # dev
-// export const baseUrl = "https://repsdev31.ue.r.appspot.com"
+export const baseUrl = "https://repsdev31.ue.r.appspot.com"
 //# prod
  // export const baseUrl = "https://repsdms.ue.r.appspot.com"
 
