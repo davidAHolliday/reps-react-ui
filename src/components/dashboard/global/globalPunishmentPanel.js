ee5c26e146e63c7678ee34204b3ebd747142aa18
castmygame <castmygameinc@gmail.com>
2024-02-29 15:43:13 -0500

    Updated button color

--- src/components/dashboard/global/globalPunishmentPanel.js
+++ src/components/dashboard/global/globalPunishmentPanel.js
@@ -204,8 +204,8 @@ const defaultTheme = createTheme();
       <button style={{backgroundColor:"red"}} onClick={() => {
         setOpenModal({display:false,message:""})
         setTextareaValue("")}}>Cancel</button>
-      {openModal.buttonType==="delete" && <button disabled={textareaValue.length===0} style={{backgroundColor: textareaValue===""?"grey":'green'}} onClick={() => handleDeletePunishment(deletePayload)}>Delete Referral</button>}
-     {openModal.buttonType==="close" && <button disabled={textareaValue.length===0} style={{backgroundColor:textareaValue===""?"grey":"white"}} onClick={() => handleClosePunishment(deletePayload)}>Close Referral</button>}
+      {openModal.buttonType==="delete" && <button disabled={textareaValue.length===0} style={{backgroundColor: textareaValue===""?"grey":'red'}} onClick={() => handleDeletePunishment(deletePayload)}>Delete Referral</button>}
+     {openModal.buttonType==="close" && <button disabled={textareaValue.length===0} style={{backgroundColor:textareaValue===""?"grey":"green"}} onClick={() => handleClosePunishment(deletePayload)}>Close Referral</button>}
 
     </div>
   </div>
