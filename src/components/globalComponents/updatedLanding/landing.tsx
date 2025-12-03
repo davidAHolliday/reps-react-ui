d9cd240cda9433d945c37589213a2c0a034233e6
castmygame <castmygameinc@gmail.com>
2025-12-02 22:52:12 -0500

    removed unused import

--- src/components/globalComponents/updatedLanding/landing.tsx
+++ src/components/globalComponents/updatedLanding/landing.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import "./landing-01.css";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import axios from "axios";
-import { baseUrl, redirectUrl } from "src/utils/jsonData";
+import { baseUrl } from "src/utils/jsonData";
 import Snackbar from "@mui/material/Snackbar";
 import Alert from "@mui/material/Alert";
 import { AccordionItem } from "./AccordianItem";
